7fd3d90a78ba85d5fcce9c6c07e00b4442640ad4
Liliana Listus <lilja.listus@gmail.com>
2019-02-25 14:44:35 +0200

    Rewrite the seach function.

--- src/common/services/search.ts
+++ src/common/services/search.ts
@@ -1,45 +1,24 @@
 import algoliasearch from 'algoliasearch';
 import { IEvent } from '../interfaces/events';
 
-// let query = ''; 
-let event: IEvent;
 let client;
 let index;
-let events: IEvent[];
 
 export async function searchClick(query): Promise<IEvent[]> {
-    events = [];
-
     client = algoliasearch('UFI1GERD33', '0f949363e44d09d51dd523165f9f36a0');
     index = client.initIndex('conferences');
 
-    return index.search(query).then(result => {
-        const hits = result.hits;
-
-        hits.forEach((element) => {
-            let links = element.links;
-            let listOfLinks = '';
-            let locationLinks = [];
-            links.forEach((link) => {
-                if (link !== '') {
-                    listOfLinks += '<a>' + link + '</a>' + ', ';
-                    locationLinks.push(listOfLinks);
-                }
-            });
-
-            event = {
-                name: element.name,
-                dates: element.start_date + ' - ' + element.end_date,
-                links: locationLinks,
-                location: element.location.country.eng + ", " + element.location.city.eng
-            }
-
-            events.push(event);
-
-
+    return index.search(query)
+        ///    
+        .then(result => {
+            const hits = result.hits || [];
+            return hits.map((hit => ({
+                name: hit.name,
+                dates: hit.start_date + ' - ' + hit.end_date,
+                links: hit.links
+                    .filter(link => link !== '')
+                    .map((link) => ({ url: link })),
+                location: hit.location.country.eng + ", " + hit.location.city.eng
+            })));
         });
-        return events;
-    });
-
-    // return events;
 }
