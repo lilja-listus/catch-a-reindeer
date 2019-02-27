dc2e869e1aba4eeec01b1e406fda3cbe3dae5488
Liliana Listus <lilja.listus@gmail.com>
2019-02-27 15:50:21 +0200

    Make toast message.

--- src/containers/SpecialCharacters/index.tsx
+++ src/containers/SpecialCharacters/index.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './index.scss';
 import { SwitchButton } from '../../components/button';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { ToastsContainer, ToastsStore } from 'react-toasts';
+
 
 
 export class SpecialCharacters extends Component<any, any>{
@@ -13,45 +15,38 @@ export class SpecialCharacters extends Component<any, any>{
         }
     }
 
-    //readonly charactersUdmKomi = ['e̮', 'i̮', 'č́', 'ć', 'č', 'ʒ́', 'ǯ', 'ń', "l' ", "d' ", 'ź', 'ś', 'š', 'ž'];
+    readonly charactersUdmKomi = ['e̮', 'i̮', 'č́', 'ć', 'č', 'ʒ́', 'ǯ', 'ń', "l'", "d'", 'ź', 'ś', 'š', 'ž'];
+    readonly charactersMari = ['č́', 'ž', 'š', 'ə̑', "l'", 'ń', 'ö', 'ü', 'ä', 'ə', 'č', 'ŋ', 'β', 'γ'];
+    readonly charactersHungarian = ['á', 'é', 'í', 'ó', 'ő', 'ú', 'ü', 'ű'];
+
 
     onCharacterClick = (character: string) => {
         this.setState({ selectedCharacter: character })
 
     }
 
-
-
     public render() {
-
         const characterToDiv = (character) => {
             return (
                 <CopyToClipboard text={this.state.selectedCharacter} options={{ debug: true }}
-                    onCopy={() => console.log(this.state.selectedCharacter)}>
+                    onCopy={() => ToastsStore.success(`${character} copied`)}>
 
                     <div onClick={(e: any) => {
                         this.setState({ selectedCharacter: character })
                     }}
                         className="character"> {character}</div>
                 </CopyToClipboard>
-
             )
         }
 
-        const charactersUdmKomi = ['e̮', 'i̮', 'č́', 'ć', 'č', 'ʒ́', 'ǯ', 'ń', "l' ", "d' ", 'ź', 'ś', 'š', 'ž'];
-        const charactersMari = ['č́ ', 'ž ', 'š ', 'ə̑ ', "l' ", 'ń ', 'ö ', 'ü ', 'ä ', 'ə ', 'č ', 'ŋ ', 'β ', 'γ'];
-        const charactersHungarian = ['á', 'é', 'í', 'ó', 'ő', 'ú', 'ü', 'ű'];
-
-        const mapUdmKomi = charactersUdmKomi.map(characterToDiv);
-        const mapMari = charactersMari.map(characterToDiv);
-        const mapHungarian = charactersHungarian.map(characterToDiv);
-
-
+        const mapUdmKomi = this.charactersUdmKomi.map(characterToDiv);
+        const mapMari = this.charactersMari.map(characterToDiv);
+        const mapHungarian = this.charactersHungarian.map(characterToDiv);
 
         return (
             <div className='special-characters'>
-                <div>Writing a paper and need some special characters?  <p>Just copy-paste them</p></div>
-
+                <ToastsContainer store={ToastsStore} />
+                <div>Writing a paper and need some special characters?  <p>Just click on one and get it co pied to your buffer</p></div>
                 <div className="text">
                     <div>Udmurtti ja komi: </div>
                     <div className="characters">{React.Children.toArray(mapUdmKomi)}</div>
@@ -60,10 +55,8 @@ export class SpecialCharacters extends Component<any, any>{
                     <div>Unkari: </div>
                     <div className="characters">{React.Children.toArray(mapHungarian)} </div>
                 </div>
-
+                <SwitchButton text={'home'} link={'/'} />
             </div>
-
-
         );
     }
 }
