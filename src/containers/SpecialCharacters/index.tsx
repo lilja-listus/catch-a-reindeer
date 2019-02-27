6e641db59e4170225b7ed162b17f522adf7aa44a
Liliana Listus <lilja.listus@gmail.com>
2019-02-27 15:28:04 +0200

    Add copy to buffer.

--- src/containers/SpecialCharacters/index.tsx
+++ src/containers/SpecialCharacters/index.tsx
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
 import './index.scss';
 import { SwitchButton } from '../../components/button';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 
 export class SpecialCharacters extends Component<any, any>{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedCharacter: ''
+        }
+    }
+
+    //readonly charactersUdmKomi = ['e̮', 'i̮', 'č́', 'ć', 'č', 'ʒ́', 'ǯ', 'ń', "l' ", "d' ", 'ź', 'ś', 'š', 'ž'];
+
+    onCharacterClick = (character: string) => {
+        this.setState({ selectedCharacter: character })
+
+    }
+
+
+
     public render() {
+
+        const characterToDiv = (character) => {
+            return (
+                <CopyToClipboard text={this.state.selectedCharacter} options={{ debug: true }}
+                    onCopy={() => console.log(this.state.selectedCharacter)}>
+
+                    <div onClick={(e: any) => {
+                        this.setState({ selectedCharacter: character })
+                    }}
+                        className="character"> {character}</div>
+                </CopyToClipboard>
+
+            )
+        }
+
         const charactersUdmKomi = ['e̮', 'i̮', 'č́', 'ć', 'č', 'ʒ́', 'ǯ', 'ń', "l' ", "d' ", 'ź', 'ś', 'š', 'ž'];
         const charactersMari = ['č́ ', 'ž ', 'š ', 'ə̑ ', "l' ", 'ń ', 'ö ', 'ü ', 'ä ', 'ə ', 'č ', 'ŋ ', 'β ', 'γ'];
         const charactersHungarian = ['á', 'é', 'í', 'ó', 'ő', 'ú', 'ü', 'ű'];
 
-        const mapUdmKomi = charactersUdmKomi.map(character => (<div className="character"> {character}</div>));
-        const mapMari = charactersMari.map(character => (<div className="character"> {character}</div>));
-        const mapHungarian = charactersHungarian.map(character => (<div className="character"> {character}</div>));
+        const mapUdmKomi = charactersUdmKomi.map(characterToDiv);
+        const mapMari = charactersMari.map(characterToDiv);
+        const mapHungarian = charactersHungarian.map(characterToDiv);
 
 
 
         return (
             <div className='special-characters'>
                 <div>Writing a paper and need some special characters?  <p>Just copy-paste them</p></div>
-
 
                 <div className="text">
                     <div>Udmurtti ja komi: </div>
@@ -27,9 +60,10 @@ export class SpecialCharacters extends Component<any, any>{
                     <div>Unkari: </div>
                     <div className="characters">{React.Children.toArray(mapHungarian)} </div>
                 </div>
-                <SwitchButton text='home' link='/' />
 
             </div>
+
+
         );
     }
 }
