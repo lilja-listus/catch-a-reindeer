840f1cc9abbade04300daa84f8546a9d36888ad6
Liliana Listus <lilja.listus@gmail.com>
2019-02-27 12:21:03 +0200

    Add Hungarian. Make charactes mapped.

--- src/containers/SpecialCharacters/index.tsx
+++ src/containers/SpecialCharacters/index.tsx
@@ -4,20 +4,28 @@ import { SwitchButton } from '../../components/button';
 
 export class SpecialCharacters extends Component<any, any>{
     public render() {
-        const charactersUdmKomi = ['e̮', 'i̮', 'č́', 'ć', 'č', 'ʒ́', 'ǯ', 'ń', 'l', '&#39;', 'd', '&#39;', 'ź', 'ś', 'š', 'ž'];
-        const charactersMari = ['č́ ', 'ž ', 'š ', 'ə̑ ', 'l&#39; ', 'ń ', 'ö ', 'ü ', 'ä ', 'ə ', 'č ', 'ŋ ', 'β ', '(γ) '];
+        const charactersUdmKomi = ['e̮', 'i̮', 'č́', 'ć', 'č', 'ʒ́', 'ǯ', 'ń', "l' ", "d' ", 'ź', 'ś', 'š', 'ž'];
+        const charactersMari = ['č́ ', 'ž ', 'š ', 'ə̑ ', "l' ", 'ń ', 'ö ', 'ü ', 'ä ', 'ə ', 'č ', 'ŋ ', 'β ', 'γ'];
+        const charactersHungarian = ['á', 'é', 'í', 'ó', 'ő', 'ú', 'ü', 'ű'];
 
-        // const divMari = (charactersMari).map(character => {
-        //     (<div>{character}</div>)
-        // });
+        const mapUdmKomi = charactersUdmKomi.map(character => (<div className="character"> {character}</div>));
+        const mapMari = charactersMari.map(character => (<div className="character"> {character}</div>));
+        const mapHungarian = charactersHungarian.map(character => (<div className="character"> {character}</div>));
 
-        // console.log(divMari);
+
 
         return (
             <div className='special-characters'>
-                <div className="text">        SUT
-                    <div>Udmurtti ja komi: {React.Children.toArray(charactersUdmKomi)}</div>
-                    <div> Mari: {React.Children.toArray(charactersMari)} </div>
+                <div>Writing a paper and need some special characters?  <p>Just click on one and it copies </p></div>
+
+
+                <div className="text">
+                    <div>Udmurtti ja komi: </div>
+                    <div className="characters">{React.Children.toArray(mapUdmKomi)}</div>
+                    <div>Mari: </div>
+                    <div className="characters"> {React.Children.toArray(mapMari)} </div>
+                    <div>Unkari: </div>
+                    <div className="characters">{React.Children.toArray(mapHungarian)} </div>
                 </div>
                 <SwitchButton text='home' link='/' />
 
