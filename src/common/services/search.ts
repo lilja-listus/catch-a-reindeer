69baafc6e3d5a310e4ec9eec7910acda6f8e03b4
Lilja Listus <lilja.listus@gmail.com>
2019-06-11 17:55:20 +0300

    Clean code from comments. Delete not needed files.

--- src/common/services/search.ts
+++ src/common/services/search.ts
@@ -1,23 +1,22 @@
-import algoliasearch from 'algoliasearch';
-import { IEvent } from '../interfaces/events';
+import algoliasearch from "algoliasearch";
+import { IEvent } from "../interfaces/events";
+import { algoliaCreds } from "../config";
 
 let client;
 let index;
 
-
 export async function searchClick(query): Promise<IEvent[]> {
-    client = algoliasearch('UFI1GERD33', '0f949363e44d09d51dd523165f9f36a0');
-    index = client.initIndex('conferences');
-    return index.search(query)
-        .then(result => {
-            const hits = result.hits || [];
-            return hits.map((hit => ({
-                name: hit.name,
-                dates: hit.start_date + ' - ' + hit.end_date,
-                links: hit.links || []
-                    .filter(link => link !== '')
-                    .map((link) => ({ url: link })),
-                location: hit.location.city.eng + " (" + hit.location.country.eng + ")"
-            })));
-        });
+  client = algoliasearch(algoliaCreds.client, algoliaCreds.key);
+  index = client.initIndex("conferences");
+  return index.search(query).then(result => {
+    const hits = result.hits || [];
+    return hits.map(hit => ({
+      name: hit.name,
+      dates: hit.start_date + " - " + hit.end_date,
+      links:
+        hit.links ||
+        [].filter(link => link !== "").map(link => ({ url: link })),
+      location: hit.location.city.eng + " (" + hit.location.country.eng + ")"
+    }));
+  });
 }
